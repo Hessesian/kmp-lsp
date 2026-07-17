7c22ca9b84b70e4c77ac7e651ed676f009f657e1
Sora Nai <hikkideveloper@gmail.com>
2026-07-17 15:31:15 +0200

    test(spec): audit Kotlin Core control flow analysis

--- src/kotlin_spec_tests/control_flow_analysis.rs
+++ src/kotlin_spec_tests/control_flow_analysis.rs
@@ -1,8 +1,8 @@
 use super::{assert_source_has_syntax_error, assert_source_parses};
 
 #[test]
-#[ignore = "KS-12.2.3-001: kmp-lsp does not diagnose path-sensitive uninitialized reads"]
-fn ks_12_2_3_001_property_must_be_assigned_on_every_reaching_path() {
+#[ignore = "KS-CDFA-0061: kmp-lsp does not diagnose path-sensitive uninitialized reads"]
+fn ks_cdfa_0061_property_must_be_assigned_on_every_reaching_path() {
     assert_source_parses(
         "fun validSpec(conditionSpec: Boolean): Int {\n    val valueSpec: Int\n    if (conditionSpec) { valueSpec = 1 } else { valueSpec = 2 }\n    return valueSpec\n}\n",
     );
@@ -12,8 +12,8 @@ fn ks_12_2_3_001_property_must_be_assigned_on_every_reaching_path() {
 }
 
 #[test]
-#[ignore = "KS-12.2.5-001: kmp-lsp does not apply calls-in-place contracts to definite assignment"]
-fn ks_12_2_5_001_run_exactly_once_contract_propagates_assignment() {
+#[ignore = "KS-CDFA-0082: kmp-lsp does not apply calls-in-place contracts to definite assignment"]
+fn ks_cdfa_0082_run_exactly_once_contract_propagates_assignment() {
     assert_source_parses(
         "fun validSpec(): Int {\n    val valueSpec: Int\n    run { valueSpec = 1 }\n    return valueSpec\n}\n",
     );
