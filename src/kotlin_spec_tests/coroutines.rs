c6f3f94e12fa8ba8f88145275182239e4e7fecbe
Sora Nai <hikkideveloper@gmail.com>
2026-07-17 15:54:09 +0200

    test(spec): audit Kotlin Core coroutines

--- src/kotlin_spec_tests/coroutines.rs
+++ src/kotlin_spec_tests/coroutines.rs
@@ -1,8 +1,8 @@
 use super::{assert_source_has_syntax_error, assert_source_parses};
 
 #[test]
-#[ignore = "KS-18.1-001: kmp-lsp does not diagnose suspend calls from non-suspending contexts"]
-fn ks_18_1_001_only_suspending_context_may_call_suspending_function() {
+#[ignore = "KS-COROUTINES-0005: kmp-lsp does not diagnose suspend calls from non-suspending contexts"]
+fn ks_coroutines_0005_only_suspending_context_may_call_suspending_function() {
     assert_source_parses(
         "suspend fun loadSpec(): String = \"value\"\nsuspend fun validSpec(): String = loadSpec()\n",
     );
