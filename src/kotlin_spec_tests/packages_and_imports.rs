57befb438c4f8be8b66f4d10b5f6a4f9f3c7b87c
Sora Nai <hikkideveloper@gmail.com>
2026-07-17 14:54:41 +0200

    test(spec): audit Kotlin Core packages

--- src/kotlin_spec_tests/packages_and_imports.rs
+++ src/kotlin_spec_tests/packages_and_imports.rs
@@ -1,14 +1,14 @@
 use super::{assert_source_has_syntax_error, assert_source_parses};
 
 #[test]
-fn ks_10_001_file_accepts_zero_or_one_simple_or_qualified_package_header() {
+fn ks_packages_0001_file_accepts_zero_or_one_package_header_and_root_package() {
     assert_source_parses("val rootSpec = 1\n");
     assert_source_parses("package sample\nval simpleSpec = 1\n");
     assert_source_parses("package sample.feature;\nval qualifiedSpec = 1\n");
 }
 
 #[test]
-fn ks_10_002_file_cannot_have_multiple_package_headers() {
+fn ks_packages_0002_file_cannot_have_multiple_package_headers() {
     assert_source_parses("package sample\nval validSpec = 1\n");
     assert_source_has_syntax_error(
         "package first.sample\npackage second.sample\nval invalidSpec = 1\n",
@@ -16,15 +16,21 @@ fn ks_10_002_file_cannot_have_multiple_package_headers() {
 }
 
 #[test]
-fn ks_10_1_001_import_directives_accept_regular_star_and_renaming_forms() {
+fn ks_packages_0008_import_directives_accept_regular_star_and_renaming_forms() {
     assert_source_parses(
         "package usage.sample\nimport source.sample.valueSpec\nimport source.sample.*\nimport source.sample.otherSpec as renamedSpec\nval resultSpec = renamedSpec\n",
     );
 }
 
 #[test]
-#[ignore = "KS-10.1-002: kmp-lsp does not reject star imports from objects"]
-fn ks_10_1_002_object_star_import_is_forbidden() {
+fn ks_packages_0009_import_directive_accepts_simple_and_qualified_paths() {
+    assert_source_parses("import valueSpec\nval simpleSpec = valueSpec\n");
+    assert_source_parses("import source.sample.valueSpec\nval qualifiedSpec = valueSpec\n");
+}
+
+#[test]
+#[ignore = "KS-PACKAGES-0015: kmp-lsp does not reject star imports from objects"]
+fn ks_packages_0015_object_star_import_is_forbidden() {
     assert_source_parses(
         "package usage.sample\nimport source.sample.ContainerSpec.memberSpec\nval validSpec = memberSpec\n",
     );
